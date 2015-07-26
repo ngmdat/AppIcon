b7fc0f11044577609eea9cb068de40ce60c78a9f
Harada TatsuAki <ngmdat@gmail.com>
2015-07-26 20:24:32 +0900

    Initial commit

---
+++ app/src/androidTest/java/com/atom_ss/appicon/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.atom_ss.appicon;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
